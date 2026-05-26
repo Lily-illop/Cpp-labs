e966c9f1d9b29e6921aac52cfb4ac0f1c3a08694
al <alenalexkuz@gmail.com>
2026-05-26 22:06:36 +0300

    upgrade: move count logic and print logic to funcs

--- labwork-1-WordCount/main.cpp
+++ labwork-1-WordCount/main.cpp
@@ -7,14 +7,26 @@
 
 using namespace std;
 
-struct Options {
+struct Flags {
   bool show_lines = false;
   bool show_words = false;
   bool show_bytes = false;
   bool show_chars = false;
+};
+
+struct Options {
+  Flags flag;
   vector<string> files;
 };
 
+struct FileStats {
+  string filename;
+  long lines = 0;
+  long words = 0;
+  long bytes = 0;
+  long chars = 0;
+  bool is_valid = false;
+};
 
 // Парсинг аргументов командной строки
 Options parse_arguments(int argc, char* argv[]) {
@@ -36,7 +48,7 @@ Options parse_arguments(int argc, char* argv[]) {
         if (arg[j] == 'l' || arg[j] == 'w' || arg[j] == 'c' || arg[j] == 'm')
           params.insert(arg[j]);
         else
-          cout << "Warning: Unknown option '-" << arg[j]
+          cerr << "Warning: Unknown option '-" << arg[j]
                << "'. It will be ignored" << endl;
       }
     } else if (arg == "--lines") {
@@ -48,24 +60,76 @@ Options parse_arguments(int argc, char* argv[]) {
     } else if (arg == "--chars") {
       params.insert('m');
     } else {
-      std::cout << "Unexpected parametr `" << arg << "`. It will be ignored"
+      cerr << "Unexpected parametr `" << arg << "`. It will be ignored"
                 << std::endl;
     }
   }
-  opts.show_lines = params.count('l');
-  opts.show_words = params.count('w');
-  opts.show_bytes = params.count('c');
-  opts.show_chars = params.count('m');
+  opts.flag.show_lines = params.count('l');
+  opts.flag.show_words = params.count('w');
+  opts.flag.show_bytes = params.count('c');
+  opts.flag.show_chars = params.count('m');
 
   // Если нет ни одной опции - выводить нужно все (кроме chars, как в wc)
-  if (!opts.show_lines && !opts.show_words && !opts.show_bytes &&
-      !opts.show_chars) {
-    opts.show_lines = true;
-    opts.show_words = true;
-    opts.show_bytes = true;
+  if (!opts.flag.show_lines && !opts.flag.show_words && !opts.flag.show_bytes &&
+      !opts.flag.show_chars) {
+    opts.flag.show_lines = true;
+    opts.flag.show_words = true;
+    opts.flag.show_bytes = true;
   }
 
   return opts;
+}
+
+FileStats count_file_stats(const string& filename) {
+  FileStats stats;
+  stats.filename = filename;
+
+  ifstream file(filename, ios::binary);
+  if (!file.is_open()) {
+    cerr << "Error: Cannot open file '" << filename << "'" << endl;
+    return stats; // return false by default
+  }
+
+  bool in_word = false;
+  char ch;
+  // Один проход по файлу, читаем посимвольно
+  while (file.get(ch)) {
+    stats.bytes++;  // Считаем байты
+    stats.chars++;  // Считаем символы (все, включая спецсимволы)
+
+    if (ch == '\n') {
+      stats.lines++;
+    }
+
+    // Подсчет слов: пробельные символы разделяют слова
+    if (isspace(static_cast<unsigned char>(ch))) {
+      if (in_word) {
+        stats.words++;
+        in_word = false;
+      }
+    } else {
+      in_word = true;
+    }
+  }
+
+  // Если файл закончился на слове
+  if (in_word) {
+    stats.words++;
+  }
+
+  stats.is_valid = true;
+  return stats;
+}
+
+void print_stats(const FileStats& stats, const Flags flags) {
+  if (!stats.is_valid) return;
+
+  if (flags.show_lines) cout << stats.lines << " ";
+  if (flags.show_words) cout << stats.words << " ";
+  if (flags.show_bytes) cout << stats.bytes << " ";
+  if (flags.show_chars) cout << stats.chars << " ";
+  
+  cout << " " << stats.filename << endl;
 }
 
 int main(int argc, char** argv) {
@@ -77,46 +141,30 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  vector<string> result = vector<string>();
+  FileStats total;
+  total.filename = "total";
+  bool has_total = false;
 
-  for (auto filename : opts.files) {
-    ifstream file(filename, ios::in);
-    long int c_lines = 0;
+  for (const string& filename : opts.files) {
+    FileStats stats = count_file_stats(filename);
 
-    long int c_words = 0;
-    long int c_chars = 0;
+    if (stats.is_valid) {
+      print_stats(stats, opts.flag);
 
-    if (file.is_open()) {
-      std::ifstream in(filename, ifstream::binary);
-      long int c_bytes = in.seekg(0, ios::end).tellg();
-      in.close();
-
-      string line;
-      string text = "";
-      while (getline(file, line)) {
-        c_lines++;
-        text += line + "\n";
+      // Суммируем для общего итога
+      if (opts.files.size() > 1) {
+        total.lines += stats.lines;
+        total.words += stats.words;
+        total.bytes += stats.bytes;
+        total.chars += stats.chars;
+        has_total = true;
       }
-      regex word_regex("(\\w+)");
-      auto words_begin = sregex_iterator(text.begin(), text.end(), word_regex);
-      auto words_end = sregex_iterator();
-      c_words = distance(words_begin, words_end);
-
-      regex word_regex1("[A-Za-z]{1}");
-      words_begin = sregex_iterator(text.begin(), text.end(), word_regex1);
-      words_end = sregex_iterator();
-      c_chars = distance(words_begin, words_end);
-
-      if (opts.show_lines) cout << c_lines << " ";
-      if (opts.show_words) cout << c_words << " ";
-      if (opts.show_bytes) cout << c_bytes << " ";
-      if (opts.show_chars) cout << c_chars << " ";
-      cout << filename << "\n";
-
-      file.close();
-    } else {
-      cout << "There is no file named \'" << filename << "\'\n";
     }
+  }
+
+  // Выводим общий итог, если было несколько файлов
+  if (has_total) {
+    print_stats(total, opts.flag);
   }
 
   return 0;
